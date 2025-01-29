f24e43323916d52246e1b9567433d28d13d4c6c5
Abdul Salaam Adeleke <78124617+abduladeleke26@users.noreply.github.com>
2025-01-29 03:30:56 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+import requests
+import base64
+from pypdf import PdfReader
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+url = "https://texttospeech.googleapis.com/v1/text:synthesize"
+
+params = {"key": os.environ.get('FLASK_KEY')}
+
+UPLOAD_FOLDER = "files"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+def tts(text):
+
+    payload = {
+        "input": {
+            "text": text
+
+        },
+        "voice": {
+            "languageCode": "en-US"
+
+        },
+        "audioConfig": {
+            "audioEncoding": "LINEAR16"
+
+        }
+
+    }
+
+    response = requests.post(url, json=payload, params=params)
+
+    get = response.json()
+
+    audio = get.get('audioContent')
+    encoded = audio.encode()
+    array = base64.b64decode(encoded)
+
+    with open('static/sound/test.wav', 'wb') as f:
+        f.write(array)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/speech', methods=['POST'])
+def speech():
+
+    file = request.files["pdf"]
+    textt = request.form.get('text')
+
+    if file and textt:
+        tts("You filled in the pdf and text. fill in only one.")
+        audio = True
+    elif textt:
+        tts(textt)
+        audio = True
+    elif file and file.filename.endswith(".pdf"):
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+        file.save(file_path)
+
+        reader = PdfReader(file)
+
+        text = ""
+        for page in reader.pages:
+            text = text + page.extract_text()
+
+        print(text)
+
+        tts(text)
+        audio = True
+
+
+
+    return render_template("index.html",text =textt, audio=audio)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
